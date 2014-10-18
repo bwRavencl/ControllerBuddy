c514d421ef6108d5e8bb02463ed8f710ce08bd4a
Matteo Hausner <matteo.hausner@gmail.com>
2014-10-18 17:33:21 +0200

    Added key/modifier code definitions Minor i18n fix

--- src/de/bwravencl/RemoteStick/input/KeyStroke.java
+++ src/de/bwravencl/RemoteStick/input/KeyStroke.java
@@ -19,11 +19,40 @@ package de.bwravencl.RemoteStick.input;
 
 public class KeyStroke {
 
-	public static final String[] KEY_CODES = { "VK_A", "VK_B", "VK_C" };
-	public static final String[] MODIFIER_CODES = { "CTRL", "ALT", "SHIFT" };
+	public static final String[] KEY_CODES = { "LBUTTON", "RBUTTON", "CANCEL",
+			"MBUTTON", "XBUTTON1", "XBUTTON2", "BACK", "TAB", "CLEAR",
+			"RETURN", "SHIFT", "CONTROL", "MENU", "PAUSE", "CAPITAL", "KANA",
+			"HANGEUL", "HANGUL", "JUNJA", "FINAL", "KANJI", "HANJA", "ESCAPE",
+			"CONVERT", "NONCONVERT", "ACCEPT", "MODECHANGE", "SPACE", "PRIOR",
+			"NEXT", "END", "HOME", "LEFT", "UP", "RIGHT", "DOWN", "SELECT",
+			"PRINT", "EXECUTE", "SNAPSHOT", "INSERT", "DELETE", "HELP", "VK_0",
+			"VK_1", "VK_2", "VK_3", "VK_4", "VK_5", "VK_6", "VK_7", "VK_8",
+			"VK_9", "VK_A", "VK_B", "VK_C", "VK_D", "VK_E", "VK_F", "VK_G",
+			"VK_H", "VK_I", "VK_J", "VK_K", "VK_L", "VK_M", "VK_N", "VK_N",
+			"VK_O", "VK_P", "VK_Q", "VK_R", "VK_S", "VK_T", "VK_U", "VK_V",
+			"VK_W", "VK_X", "VK_Y", "VK_Z", "LWIN", "RWIN", "APPS", "SLEEP",
+			"NUMPAD0", "NUMPAD1", "NUMPAD2", "NUMPAD3", "NUMPAD4", "NUMPAD5",
+			"NUMPAD6", "NUMPAD7", "NUMPAD8", "NUMPAD9", "MULTIPLY", "ADD",
+			"SEPARATOR", "SUBTRACT", "DECIMAL", "DIVIDE", "F1", "F2", "F3",
+			"F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13",
+			"F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21", "F22",
+			"F23", "F24", "NUMLOCK", "SCROLL", "LSHIFT", "RSHIFT", "LCONTROL",
+			"RCONTROL", "LMENU", "RMENU", "BROWSER_BACK", "BROWSER_FORWARD",
+			"BROWSER_REFRESH", "BROWSER_STOP", "BROWSER_SEARCH",
+			"BROWSER_FAVORITES", "BROWSER_HOME", "VOLUME_MUTE", "VOLUME_DOWN",
+			"VOLUME_UP", "MEDIA_NEXT_TRACK", "MEDIA_PREV_TRACK", "MEDIA_STOP",
+			"MEDIA_PLAY_PAUSE", "LAUNCH_MAIL", "LAUNCH_MEDIA_SELECT",
+			"LAUNCH_APP1", "LAUNCH_APP2", "OEM_1", "OEM_PLUS", "OEM_COMMA",
+			"OEM_MINUS", "OEM_PERIOD", "OEM_2", "OEM_2", "OEM_3", "OEM_4",
+			"OEM_5", "OEM_6", "OEM_7", "OEM_8", "OEM_102", "PROCESSKEY",
+			"PACKET", "ATTN", "CRSEL", "EXSEL", "EREOF", "PLAY", "ZOOM",
+			"NONAME", "PA1", "OEM_CLEAR" };
+	public static final String[] MODIFIER_CODES = { "SHIFT", "CONTROL", "MENU",
+			"LSHIFT", "RSHIFT", "LCONTROL", "RCONTROL", "LMENU", "RMENU",
+			"LWIN", "RWIN" };
 
-	private String[] keyCodes = new String[0];
-	private String[] modifierCodes = new String[0];
+	private String[] keyCodes = {};
+	private String[] modifierCodes = {};
 
 	public String[] getKeyCodes() {
 		return keyCodes;
