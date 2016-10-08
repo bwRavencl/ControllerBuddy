bwRavencl/ControllerBuddy

commit c74e3c84213e6b87fb29679d4bfc7544a409fbb3

Added resetting of non-relative axis on mode switching in case the component in question is used differently under the new mode

src/main/java/de/bwravencl/controllerbuddy/input/Profile.java
@@ -28,7 +28,10 @@ import java.util.UUID;
 
 import de.bwravencl.controllerbuddy.gui.Main;
 import de.bwravencl.controllerbuddy.input.Input.VirtualAxis;
+import de.bwravencl.controllerbuddy.input.action.AxisToAxisAction;
+import de.bwravencl.controllerbuddy.input.action.AxisToRelativeAxisAction;
 import de.bwravencl.controllerbuddy.input.action.ButtonToModeAction;
+import de.bwravencl.controllerbuddy.input.action.IAction;
 import net.brockmatt.util.ResourceBundleUtil;
 
 public class Profile implements Cloneable {
@@ -123,17 +126,32 @@ public class Profile implements Cloneable {
 		modes.remove(mode);
 	}
 
-	public void setActiveMode(int index) {
+	public void setActiveMode(Input input, int index) {
 		if (modes.size() > index) {
+			final Mode newMode = modes.get(index);
+
+			for (final String c : newMode.getComponentToActionsMap().keySet()) {
+				final Map<String, List<IAction>> currentComponentToActionsMap = getActiveMode()
+						.getComponentToActionsMap();
+				if (currentComponentToActionsMap.containsKey(c)) {
+					for (final IAction a : currentComponentToActionsMap.get(c)) {
+						if (a instanceof AxisToAxisAction && !(a instanceof AxisToRelativeAxisAction)) {
+							final AxisToAxisAction axisToAxisAction = (AxisToAxisAction) a;
+							input.setAxis(axisToAxisAction.getVirtualAxis(), 0.0f);
+						}
+					}
+				}
+			}
+
 			activeModeIndex = index;
-			Main.setOverlayText(modes.get(index).getDescription());
+			Main.setOverlayText(newMode.getDescription());
 		}
 	}
 
-	public void setActiveMode(UUID modeUuid) {
+	public void setActiveMode(Input input, UUID modeUuid) {
 		for (final Mode m : modes) {
 			if (m.getUuid().equals(modeUuid)) {
-				setActiveMode(modes.indexOf(m));
+				setActiveMode(input, modes.indexOf(m));
 				break;
 			}
 		}

src/main/java/de/bwravencl/controllerbuddy/input/Input.java
@@ -343,7 +343,7 @@ public class Input {
 	}
 
 	public void reset() {
-		profile.setActiveMode(0);
+		profile.setActiveMode(this, 0);
 		ButtonToModeAction.getButtonToModeActionStack().clear();
 
 		for (final Mode m : profile.getModes()) {

src/main/java/de/bwravencl/controllerbuddy/input/action/ButtonToModeAction.java
@@ -52,10 +52,10 @@ public class ButtonToModeAction implements IButtonToAction {
 			modeUuid = modes.get(1).getUuid();
 	}
 
-	private void activateMode(Profile profile) {
+	private void activateMode(Input input, Profile profile) {
 		if (!buttonToModeActionStack.contains(this)) {
 			buttonToModeActionStack.push(this);
-			profile.setActiveMode(modeUuid);
+			profile.setActiveMode(input, modeUuid);
 		}
 	}
 
@@ -126,7 +126,7 @@ public class ButtonToModeAction implements IButtonToAction {
 				}
 			}
 
-			profile.setActiveMode(previousMode.getUuid());
+			profile.setActiveMode(input, previousMode.getUuid());
 			input.getDownKeyStrokes().clear();
 		}
 	}
@@ -147,12 +147,12 @@ public class ButtonToModeAction implements IButtonToAction {
 					if (profile.getActiveMode().getUuid().equals(modeUuid))
 						deactivateMode(input, profile);
 					else if (Profile.isDefaultMode(profile.getActiveMode()) || componentNotUsedByActiveModes(input))
-						activateMode(profile);
+						activateMode(input, profile);
 
 					up = false;
 				}
 			} else if (Profile.isDefaultMode(profile.getActiveMode()) || componentNotUsedByActiveModes(input))
-				activateMode(profile);
+				activateMode(input, profile);
 		}
 	}
 

commit 746c0c17bd81a845f417ac8ce11ef29c1511c713

Fixed null pointer bug introduced with c74e3c84213e6b87fb29679d4bfc7544a409fbb3

src/main/java/de/bwravencl/controllerbuddy/input/Profile.java
@@ -130,14 +130,16 @@ public class Profile implements Cloneable {
 		if (modes.size() > index) {
 			final Mode newMode = modes.get(index);
 
-			for (final String c : newMode.getComponentToActionsMap().keySet()) {
-				final Map<String, List<IAction>> currentComponentToActionsMap = getActiveMode()
-						.getComponentToActionsMap();
-				if (currentComponentToActionsMap.containsKey(c)) {
-					for (final IAction a : currentComponentToActionsMap.get(c)) {
-						if (a instanceof AxisToAxisAction && !(a instanceof AxisToRelativeAxisAction)) {
-							final AxisToAxisAction axisToAxisAction = (AxisToAxisAction) a;
-							input.setAxis(axisToAxisAction.getVirtualAxis(), 0.0f);
+			if (input.getOutputThread() != null) {
+				for (final String c : newMode.getComponentToActionsMap().keySet()) {
+					final Map<String, List<IAction>> currentComponentToActionsMap = getActiveMode()
+							.getComponentToActionsMap();
+					if (currentComponentToActionsMap.containsKey(c)) {
+						for (final IAction a : currentComponentToActionsMap.get(c)) {
+							if (a instanceof AxisToAxisAction && !(a instanceof AxisToRelativeAxisAction)) {
+								final AxisToAxisAction axisToAxisAction = (AxisToAxisAction) a;
+								input.setAxis(axisToAxisAction.getVirtualAxis(), 0.0f);
+							}
 						}
 					}
 				}