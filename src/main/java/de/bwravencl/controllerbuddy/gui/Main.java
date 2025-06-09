008066e272c74e285d3b8f7f6dfe2388b42ef1cf
Matteo Hausner <matteo.hausner@gmail.com>
2025-06-10 01:00:15 +0200

    feat: set 'Circle to Square Mapping' to enabled  by default
    This reverts commit 2838a00abd228d4bbb5c325f8ccacb8e51020851.

--- src/main/java/de/bwravencl/controllerbuddy/gui/Main.java
+++ src/main/java/de/bwravencl/controllerbuddy/gui/Main.java
@@ -2398,7 +2398,7 @@ public final class Main {
 	}
 
 	public boolean isMapCircularAxesToSquareAxes() {
-		return preferences.getBoolean(PREFERENCES_MAP_CIRCULAR_AXES_TO_SQUARE, false);
+		return preferences.getBoolean(PREFERENCES_MAP_CIRCULAR_AXES_TO_SQUARE, true);
 	}
 
 	boolean isOverlayInLowerHalf(final Rectangle totalDisplayBounds) {
