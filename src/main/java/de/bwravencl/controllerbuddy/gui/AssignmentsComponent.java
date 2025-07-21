bwRavencl/ControllerBuddy

commit 91a12d4ee6541c4cc39ecb8d9ddfd623fc05da39

fix: also show tooltip for northern and western CompoundButtons

src/main/java/de/bwravencl/controllerbuddy/gui/AssignmentsComponent.java
@@ -328,7 +328,7 @@ final class AssignmentsComponent extends JScrollPane {
 				}
 				default -> throw buildInvalidComponentIndexException(componentType, componentIndex);
 				}
-			} else if (peer != null) {
+			} else {
 				switch (componentIndex) {
 				case SDLGamepad.SDL_GAMEPAD_AXIS_LEFTX -> setAction(new EditComponentAction(main,
 						Main.STRINGS.getString(swapLeftAndRightSticks ? "RIGHT_STICK_X_AXIS" : "LEFT_STICK_X_AXIS"),

commit bc83ce216bdcd0c275c2cc6f99f466f0dc2199c6

fix: EditActionsDialog would open twice if opened via CompoundButton
This is a regression that got introduced with commit 91a12d4ee6541c4cc39ecb8d9ddfd623fc05da39

src/main/java/de/bwravencl/controllerbuddy/gui/AssignmentsComponent.java
@@ -303,6 +303,7 @@ final class AssignmentsComponent extends JScrollPane {
 			if (peer != null) {
 				peer.setPeer(this);
 				setModel(peer.getModel());
+				model.removeActionListener(actionListener);
 			}
 
 			final var componentType = component.getType();