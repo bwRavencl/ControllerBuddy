bwRavencl/ControllerBuddy

commit 419bca54be8f553b3d88506b1d88c9e369f043b5

fix: detection of jpackage task on task graph
Note: apparently `TaskExecutionGraph.hasTask()` does no longer work as
expected - this commit replaces it by manually looking at `allTasks`.

build.gradle.kts
@@ -556,12 +556,12 @@ tasks.withType<JavaCompile>().configureEach {
   options.encoding = StandardCharsets.UTF_8.displayName()
 
   gradle.taskGraph.whenReady {
-    if (hasTask("jpackage")) {
+    if (allTasks.any { it.name == "jpackage" }) {
       options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:-preview", "-Werror"))
       options.isDeprecation = true
       options.errorprone.error("MissingBraces")
     } else {
-      options.errorprone.isEnabled = false
+      options.errorprone.isEnabled = true
     }
   }
 }

commit 55fdb07acd3834dfa307b07eda67ccaeeaef4c34

fix: disable errorprone when not building for jpackage
This is a regression that was accidentally introduced with 419bca54be8f553b3d88506b1d88c9e369f043b5

build.gradle.kts
@@ -628,7 +628,7 @@ tasks.withType<JavaCompile>().configureEach {
       options.isDeprecation = true
       options.errorprone.error("MissingBraces")
     } else {
-      options.errorprone.enabled = true
+      options.errorprone.enabled = false
     }
   }
 }